6561ac37dcd23a3033877c81836b9f717fccc648
Dale Peakall <dale@peakall.net>
2019-04-08 16:40:09 +0100

    PRMT-200: added script to support converting Splunk data to JSON that can be included in the markdown

---
+++ convert-splunk-extract-to-data.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ -z $1 ]; then
+  echo "Must specify file"
+  echo "Usage: $0 <file>"
+  exit 1
+fi
+
+cat "$1" | jq -n 'def keyfield: [inputs][0].result | to_entries | .[0].key; [[inputs] | .[] | .result | with_entries(if (.value | test("\\d+")) then {"key":.key, "value":.value|tonumber} else {"key":.key, "value":.value} end)]'
