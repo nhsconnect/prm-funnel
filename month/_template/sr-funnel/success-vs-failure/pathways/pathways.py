002a8a626a236825a3bc2bdaa08b84b4e969c758
jdunleav <jdunleav@thoughtworks.com>
2020-01-16 16:01:33 +0000

    PRMT-853 Added some python scripts that help with mangling the data from splunk

---
+++ month/_template/sr-funnel/success-vs-failure/pathways/pathways.py
@@ -0,0 +1,31 @@
+import json
+
+{"pathway":"EMIS-EMIS","success":"126156","failure":"708","Total":"126864"}
+
+contract = {
+    "pathway":str,
+    "success":int,
+    "failure":int,
+    "Total":int,
+}
+
+with open("pathways.json", "r") as read_file:
+    data = json.load(read_file)
+
+result = []
+for eachItem in data["data"]:
+    line = eachItem["result"]
+    newLine = {}
+    for key in contract.keys():
+        try: 
+            newLine[key] = contract[key](line[key])
+        except:
+            if key == "pathway":
+                newLine["pathway"] = "Unknown"
+            else: 
+                print("failed on key: '{0}'".format(key))
+                print(newLine)
+                print(line)
+    result.append(newLine) 
+
+print(json.dumps(result, indent=2))
